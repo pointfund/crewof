f442bd5240ae7f9fab037325e4cf497ac0e9af75
MX <mxselleck@gmail.com>
2018-08-01 03:23:34 -0500

    add boostrap and jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+//= require jquery
+//= require jquery_ujs
+
+//= require bootstrap-sprockets
+
+
+$(".place").css("border","1px solid red")
