ab7337cb35ca7b1507092f5526dcb0c489df7289
MX <mxselleck@gmail.com>
2018-08-02 00:13:05 -0500

    remove all

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,15 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
 
-//= require jquery
-
-
-//= require bootstrap-sprockets
 
 
 $(".place").css("border","1px solid red")
