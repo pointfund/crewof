f8b6472d87180da21b6b0947252166b3bba980a1
MX <mxselleck@gmail.com>
2018-08-02 03:53:54 -0500

    reset re order sprockets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,10 +9,15 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+//= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
 
 
 
-
-
-
-// $(".place").css("border","1px solid red")
+$(".place").css("border","1px solid red")
